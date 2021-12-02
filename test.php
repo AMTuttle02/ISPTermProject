83b8e104ad08a1eb06156511bccb0d6df786a1db
Vinnie-Windows <vrg6@uakron.edu>
2021-12-02 17:11:18 -0500

    seems like code is working for the momen

--- test.php
+++ test.php
@@ -1,14 +1,36 @@
 <?php
+// note, could use hidden fields to get the question information if needed
 
+// prints out an associative array
+function print_assoc($array)
+{
+   foreach ($array as $key => $value) 
+   {
+       echo "Key: $key; Value: $value"."<br>";
+   }
+}
 
-/*
+// pass this $key from a post request coming from script.js. Will be formatted as questionXXX-
+// will return the question plus all digits associated with it
+function get_question($key)
+{
+    $firstIndex = 0;
+    $lastIndex = 0;
 
-for sql, go through each question and find the number and add "questionX"
-make a hidden field to grab question from hidden field with post request 
+    $extractedQuestion = "question";
+    $questionNumber = "";
 
-1 loop to get all the questions [remove after]
-1 loop to get all the answers 
-*/
+    // get the question number
+    $firstIndex = strpos(strval($key),strval("question")) + 8;
+    $lastIndex = strpos(strval($key),strval("-"));
+    
+    for($i = $firstIndex; $i < $lastIndex; $i++)
+    {
+        $extractedQuestion = $extractedQuestion . htmlspecialchars($key)[$i];
+    }
+
+    return $extractedQuestion;
+}
 
 /*
 $clientHost = "localhost";
@@ -21,49 +43,49 @@ $tableName = "answers_Table";
 $conn = mysqli_connect($clientHost, $clientUsrName, $clientPassWord,$clientDBname);
 */
 
+$email = "";
+
 $extractedQuestion = "";
 $previousExtractedQuestion = "";
+
 $extractedKey = "";
-$firstIndex = 0;
-$lastIndex = 0;
 
-$valueArray = array(); //("hello, goodbye, pudding,", "cats, dogs, frogs", "sheep, cows, turkey")
+$sqlData = array();
 
-$sqlCols = "";
-$sqlValString = "";
-
+// loop through each post request and get names of questions, possibly email in a bit 
+// this is to begin building an associative array of questions and emails, to create an SQL query
 foreach ($_POST as $key => $value) 
 {
-    
+    $extractedQuestion = get_question($key);
 
-    $firstIndex = strpos(strval($key),strval("question")) + 8;
-    $lastIndex = strpos(strval($key),strval("-"));
-
-    $extractedQuestion = "question";
-    
-    for($i = $firstIndex; $i < $lastIndex; $i++)
-    {
-        $extractedQuestion = $extractedQuestion . htmlspecialchars($key)[$i];
-    }
-    
-    $extractedKey = strval($value);
-    //echo $extractedKey;
-    // detect if 
+    // detect if we change question forms
     if($previousExtractedQuestion != $extractedQuestion)
     {
-        $sqlValString = $sqlValString.$extractedKey.",";
-        //$valueArray.push($sqlValString);
-        array_push($valueArray, $sqlValString);
-        $sqlValString = "";
-        $sqlCols = $sqlCols.strval($extractedQuestion).","; // add question to the stack
+        $sqlData[strval($extractedQuestion)] = "";
         $previousExtractedQuestion = $extractedQuestion;
     }
-    else 
-    {
-        $sqlValString = $sqlValString.$extractedKey.",";
-        //echo $sqlValString;
-    }
 }
+
+// now we simply match questions to their respective portion of the associative array, 
+// and build it into a string that will later be inserted into sql
+foreach ($_POST as $key => $value) 
+{
+    $extractedQuestion = get_question($key);
+    $extractedKey = strval($value);
+
+    $sqlData[$extractedQuestion] = $sqlData[$extractedQuestion].$extractedKey.",";
+}
+
+// now we remove the comma from the end of each associative array string, so we can simply drop
+// the data into SQL  
+foreach ($sqlData as $key => $value)
+{
+    $sqlData[$key] = substr($sqlData[$key], 0, -1);
+}
+
+
+print_assoc($sqlData);
+
 
 
 
